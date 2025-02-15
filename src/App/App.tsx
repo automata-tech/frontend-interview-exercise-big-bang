404b395a8ede0ef2a3cfc04e6bfac37e5ad3fcf1
Daniel Atebije <dannytebj@gmail.com>
2025-02-15 09:13:22 +0000

    (chore): setup app - extend lint config - add tailwindcss

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,16 +3,20 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://automata.tech/" target="_blank">
-          <img src={String(automataLogo)} className="logo automata" alt="Automata logo"/>
+    <div className="container mx-auto px-4 ">
+      <div className="flex">
+        <a href="https://automata.tech/" target="_blank" rel="noreferrer">
+          <img
+            src={String(automataLogo)}
+            className="w-16 md:w-32 lg:w-48"
+            alt="Automata logo"
+          />
         </a>
       </div>
       <h1>Frontend Exercise</h1>
       <h2>Rock, Paper, Scissors, Lizard, Spock</h2>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
